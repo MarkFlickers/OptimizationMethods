ac7f304e08ff59fca07597942725c1afcce9f5e5
chasovskih <chasovskih@alphachip.ru>
2025-12-02 17:24:03 +0300

    Fix: src init.py

--- GroupProblem/True_Python/src/__init__.py
+++ GroupProblem/True_Python/src/__init__.py
@@ -8,7 +8,17 @@ Packets:
 __version__ = "1.3.3.7"
 
 from .BranchIntegrity import BranchIntegrity
+from .BranchIntegrity import OrderProcessor
+from .BranchIntegrity import BranchProcessor
 from .temp import PyVersion
 from .astar import AStarSolver
+from .astar import TreeState
 
-__all__ = ['BranchIntegrity', 'AStarSolver', 'PyVersion']
+__all__ = [
+    'BranchIntegrity',
+    'AStarSolver',
+    'PyVersion',
+    'TreeState',
+    'OrderProcessor',
+    'BranchProcessor'
+    ]
